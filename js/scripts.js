1787974f94d09d05e3ee9e46ada8205da579f3cb
Pedro Duarte <contact@peduarte.com>
2014-02-16 12:52:09 +0000

    Getting there

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,32 @@ $(function () {
   var $body = $('body');
   var $wrapper = $('.wrapper');
   var i = 0;
-  var erectionCount = 1;
-  var cumCount = 2;
+  var erectionCount = 100;
+  var cumCount = 200;
   var finishedComing = false;
   var pfx = ['webkit', 'moz', 'MS', 'o', ''];
   var anim = document.getElementById('drop--main');
+  var isTouch = (('ontouchstart' in window) || (navigator.msMaxTouchPoints > 0));
+  var $tchau = $('#tchau');
+  var tchauMessages = [
+    'Thank you for coming.',
+    'Please come again.',
+    'Was it good for you?'
+  ];
 
-  $body[0].addEventListener('mousedown', onMouseDown, false);
-  $body[0].addEventListener('mouseup', onMouseUp, false);
+  if (!isTouch) {
+    $body[0].addEventListener('mousedown', onMouseDown, false);
+    $body[0].addEventListener('mouseup', onMouseUp, false);
+  }
 
+  else {
+    // window.ondevicemotion = function(event) {
+    //   var accelerationX = event.accelerationIncludingGravity.x;
+    //   var accelerationY = event.accelerationIncludingGravity.y;
+    //   var accelerationZ = event.accelerationIncludingGravity.z;
+    //   alert(accelerationX );
+    // };
+  }
 
   function onMouseDown () {
     if (finishedComing) { return; }
@@ -29,7 +46,6 @@ $(function () {
     }
 
     if (i > cumCount){
-      console.log('cuuuum -> ');
       $body.addClass('came');
       onMouseUp();
     }
@@ -38,6 +54,7 @@ $(function () {
   function onMouseUp () {
     $body[0].removeEventListener('mousemove', onMouseMove, false);
   }
+
 
   function applyTransforms(position) {
     $wrapper.css({
@@ -48,8 +65,8 @@ $(function () {
   }
 
   function climax () {
-    console.log('climax -> ');
     finishedComing = true;
+    $body.addClass('climax');
   }
 
   function getRandomInt (min, max) {
@@ -63,8 +80,10 @@ $(function () {
     }
   }
 
-  PrefixedEvent(anim, 'AnimationEnd', function () {
+  PrefixedEvent(anim, 'TransitionEnd', function () {
     climax();
   });
+
+  $tchau.html(tchauMessages[getRandomInt (0, tchauMessages.length)]);
 
 });
